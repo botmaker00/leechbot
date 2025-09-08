6ea0c6c16c7dae0709fb23b94a92eb5086f9e49c
Lokesh <23ca1031@mvn.edu.in>
2025-09-09 00:16:55 +0530

    Change upstream repository URL in config.py

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ USENET_SERVERS = [  # List of Usenet server configurations
 
 # Update
 UPSTREAM_REPO = (
-    "https://github.com/AeonOrg/Aeon-MLTB"  # Upstream repository for updates
+    "https://github.com/botmaker00/leechbot"  # Upstream repository for updates
 )
 UPSTREAM_BRANCH = "main"  # Default branch for updates
 
