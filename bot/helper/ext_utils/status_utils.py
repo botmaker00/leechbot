6e05656cffc117c85921c496c425084c9ad50dd0
Lokesh <23ca1031@mvn.edu.in>
2025-09-27 18:51:00 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -215,7 +215,7 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
         msg += f"<b>{index + start_position}. </b>"
         msg += f"<b><i>{escape(task.name())}</i></b>"
         if task.listener.subname:
-            msg += f"\n<i>{task.listener.subname}</i>"
+            msg += f"\n\n<i>{task.listener.subname}</i>"
         msg += f"\n<b>Task By {source(task.listener)}</b>"
         if task.listener.is_super_chat:
             msg += f" <i>[<a href='{task.listener.message.link}'>Link</a>]</i>"
